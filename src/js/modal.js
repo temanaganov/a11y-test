c2f5dfad50e522dae87a8a15be3ba005910fcf9e
Artem Naganov <naganov.a@future-group.ru>
2021-02-18 21:10:45 +0300

    Commit

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,13 +1,49 @@
 const modal = document.querySelector('.modal');
+const input = document.querySelector('.modal input');
 const openModalButton = document.querySelector('.js-open-modal');
 const closeModalButton = document.querySelector('.modal__close');
 
 openModalButton.addEventListener('click', () => {
 	modal.classList.add('modal--active');
 	modal.focus();
-})
+});
 
 closeModalButton.addEventListener('click', () => {
 	modal.classList.remove('modal--active');
 	openModalButton.focus();
-})
+});
+
+modal.addEventListener('keyup', e => {
+	if (e.code === 'Escape') {
+		modal.classList.remove('modal--active');
+		openModalButton.focus();
+	}
+});
+
+////////////////////////////////////////////////////////////////
+
+const focusableEls = modal.querySelectorAll(
+	'a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input:not([disabled]), select:not([disabled])'
+);
+const firstFocusableEl = focusableEls[0];
+const lastFocusableEl = focusableEls[focusableEls.length - 1];
+
+modal.addEventListener('keydown', function (e) {
+	const isTabPressed = e.key === 'Tab';
+
+	if (!isTabPressed) {
+		return;
+	}
+
+	if (e.shiftKey) {
+		if (document.activeElement === firstFocusableEl) {
+			lastFocusableEl.focus();
+			e.preventDefault();
+		}
+	} else {
+		if (document.activeElement === lastFocusableEl) {
+			firstFocusableEl.focus();
+			e.preventDefault();
+		}
+	}
+});
